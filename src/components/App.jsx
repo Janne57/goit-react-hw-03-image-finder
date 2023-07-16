3abbd5e34b60da08f4af9c0777db3dbddf3ad057
Janne57 <evgeniyahome1981@gmail.com>
2023-07-16 15:28:39 +0300

    DidUpdate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import Searchbar from './Searchbar/Searchbar.js';
-import ImageGallery from './/ImageGallery/ImageGallery.js';
+// import ImageGallery from './/ImageGallery/ImageGallery.js';
 import * as ImageService from '..//service/img-service.js';
 
 export default class App extends Component {
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,26 +1,26 @@
-import { Component } from 'react';
-import { findRenderedComponentWithType } from 'react-dom/test-utils';
-import css from '../ImageGallery/ImageGallery.module.css';
+// import { Component } from 'react';
+// import { findRenderedComponentWithType } from 'react-dom/test-utils';
+// import css from '../ImageGallery/ImageGallery.module.css';
 
-export default class ImageGallery extends Component {
-  state = {
-    query: null,
-    loading: false,
-  };
+// export default class ImageGallery extends Component {
+//   state = {
+//     query: null,
+//     loading: false,
+//   };
 
 
-  render() {
-    const {query, loading} = this.state;
+//   render() {
+//     const {query, loading} = this.state;
 
-    return (
-      <div> {loading && <div>Loading....</div>}
-      <ul className="gallery">
-        {/* <ImageGalleryItem /> */}
-        {/* {!this.state.query && <li>{this.props.query}</li>} */}
-        <li>{this.props.query}</li>
-        {/* <li>{this.state.query}</li> */}
-      </ul>
-      </div>
-    );
-  }
-}
+//     return (
+//       <div> {loading && <div>Loading....</div>}
+//       <ul className="gallery">
+//         {/* <ImageGalleryItem /> */}
+//         {/* {!this.state.query && <li>{this.props.query}</li>} */}
+//         <li>{this.props.query}</li>
+//         {/* <li>{this.state.query}</li> */}
+//       </ul>
+//       </div>
+//     );
+//   }
+// }
